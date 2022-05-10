7016ca28344b09c94537ffc5a5cfb79fab2d929f
Jeff Day <jeffday@gmail.com>
2022-05-10 01:53:54 -0500

    doesnt do much good if i set up the weapon and attachments then dont actually spawn it

--- dayzOffline.chernarusplus/init.c
+++ dayzOffline.chernarusplus/init.c
@@ -747,6 +747,8 @@ class CustomMission: MissionServer
         string dmrRifle = "TTC_HK417";
         autoptr TStringArray dmrAttachments = {"WE_AMSOptic","TTC_M14Suppressor","TTC_DMR_AFG","TTC_ButtstockHK_Black","TTC_HK417_Magazine_20rnd"};
 
+        spawnItemWithAttachments(player, dmrRifle, dmrAttachments);
+
         autoptr TStringArray sniperAmmo = {"AmmoBox_762x39_20Rnd","AmmoBox_762x39_20Rnd","AmmoBox_762x39_20Rnd","AmmoBox_762x39_20Rnd","AmmoBox_762x39_20Rnd","AmmoBox_762x39_20Rnd","AmmoBox_762x39_20Rnd","AmmoBox_762x39_20Rnd","AmmoBox_762x39_20Rnd","AmmoBox_762x39_20Rnd"};
         autoptr TStringArray dmrAmmo = {"AmmoBox_308Win_20Rnd","AmmoBox_308Win_20Rnd","AmmoBox_308Win_20Rnd","AmmoBox_308Win_20Rnd","AmmoBox_308Win_20Rnd","AmmoBox_308Win_20Rnd","AmmoBox_308Win_20Rnd","AmmoBox_308Win_20Rnd","AmmoBox_308Win_20Rnd","AmmoBox_308Win_20Rnd"};
 
