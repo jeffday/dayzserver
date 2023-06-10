c9ddaa76f26ac0e9072bed72c68f82a5e5e9182f
Jeff Day <jeffday@gmail.com>
2023-06-10 14:50:56 -0500

    remove other vanilla stragglers i don't want to spawn, set my starting clothes in init.c

--- dayz.Lux/dayz.Lux/init.c
+++ dayz.Lux/dayz.Lux/init.c
@@ -44,16 +44,28 @@ class CustomMission: MissionServer
 		}
 	}
 
+	void spawnItemsOnPlayer(PlayerBase player, TStringArray items) {
+        for(int i = 0; i < items.Count(); i++) {
+             player.GetInventory().CreateInInventory(items[i]);
+        }
+    }
+
 	override PlayerBase CreateCharacter(PlayerIdentity identity, vector pos, ParamsReadContext ctx, string characterName)
 	{
 		Entity playerEnt;
-		playerEnt = GetGame().CreatePlayer( identity, characterName, pos, 0, "NONE" );
-		Class.CastTo( m_player, playerEnt );
+		playerEnt = GetGame().CreatePlayer(identity, characterName, pos, 0, "NONE");//Creates random player
+		Class.CastTo(m_player, playerEnt);
 
-		GetGame().SelectPlayer( identity, m_player );
+		GetGame().SelectPlayer(identity, m_player);
 
 		return m_player;
 	}
+
+    void SpawnJeffSetup(PlayerBase player) {
+        autoptr TStringArray clothes = {"ThickFramesGlasses","Shirt_Hawaiian2","Jeans_BlueDark","SK8_Sneakers_Blue"};
+
+        spawnItemsOnPlayer(player, clothes);
+    }
 
 	override void StartingEquipSetup(PlayerBase player, bool clothesChosen)
 	{
@@ -65,23 +77,13 @@ class CustomMission: MissionServer
 		itemClothing = player.FindAttachmentBySlotName( "Body" );
 		if ( itemClothing )
 		{
-			SetRandomHealth( itemClothing );
-			
-			itemEnt = itemClothing.GetInventory().CreateInInventory( "BandageDressing" );
-			if ( Class.CastTo( itemBs, itemEnt ) )
-				itemBs.SetQuantity( 2 );
-
-			string chemlightArray[] = { "Chemlight_White", "Chemlight_Yellow", "Chemlight_Green", "Chemlight_Red" };
-			int rndIndex = Math.RandomInt( 0, 4 );
-			itemEnt = itemClothing.GetInventory().CreateInInventory( chemlightArray[rndIndex] );
-			SetRandomHealth( itemEnt );
-		}
-		
-		itemClothing = player.FindAttachmentBySlotName( "Legs" );
-		if ( itemClothing )
-			SetRandomHealth( itemClothing );
-		
-		itemClothing = player.FindAttachmentBySlotName( "Feet" );
+			player.RemoveAllItems();
+            
+            SpawnJeffSetup(player);
+            
+            itemEnt = player.GetInventory().CreateInInventory( "BandageDressing" );
+			itemEnt = player.GetInventory().CreateInInventory( "Chemlight_White" );
+		} 
 	}
 };
 
