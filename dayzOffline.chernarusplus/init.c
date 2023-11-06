23889bbbb3a75d83347fb7d05f6e79a9baf48248
Jeff Day <jeffday@gmail.com>
2023-11-05 18:02:02 -0600

    remove namalsk/evr-specific init block and revert to normal-ass starting gear

--- dayzOffline.chernarusplus/init.c
+++ dayzOffline.chernarusplus/init.c
@@ -32,25 +32,6 @@ void main()
 
 class CustomMission: MissionServer
 {
-	override void OnInit()
-	{
-		super.OnInit();
-
-		// this piece of code is recommended otherwise event system is switched on automatically and runs from default values
-		// comment this whole block if NOT using Namalsk Survival
-		if ( m_EventManagerServer )
-		{
-			// enable/disable event system, min time between events, max time between events, max number of events at the same time
-			m_EventManagerServer.OnInitServer( true, 550, 1000, 2 );
-			// registering events and their probability
-			m_EventManagerServer.RegisterEvent( Aurora, 0.01 );
-			m_EventManagerServer.RegisterEvent( Blizzard, 0.0 );
-			m_EventManagerServer.RegisterEvent( ExtremeCold, 0.0 );
-			m_EventManagerServer.RegisterEvent( Snowfall, 0.0 );
-			m_EventManagerServer.RegisterEvent( EVRStorm, 0.99 );
-			m_EventManagerServer.RegisterEvent( HeavyFog, 0.0 );
-		}
-	}
 
 	void SetRandomHealth(EntityAI itemEnt)
 	{
@@ -107,14 +88,10 @@ class CustomMission: MissionServer
     void SpawnJeffSetup(PlayerBase player) {
 		EntityAI bandages;
 
-		player.GetInventory().CreateInInventory("BeanieHat_Red");
-
 		player.GetInventory().CreateInInventory("ThickFramesGlasses");
-		player.GetInventory().CreateInInventory("ParamedicJacket_Crimson");
-		player.GetInventory().CreateInInventory("ParamedicPants_Crimson");
-		player.GetInventory().CreateInInventory("YRTSK_VansSneakers_Red");
-		player.GetInventory().CreateInInventory("SurgicalGloves_Blue");
-		player.GetInventory().CreateInInventory("CanvasBag_Medical");
+		player.GetInventory().CreateInInventory("Shirt_PlaneBlack");
+		player.GetInventory().CreateInInventory("Skinny_Jeans_Blue");
+		player.GetInventory().CreateInInventory("SK8_Sneakers_Black");
 
 		bandages = player.GetInventory().CreateInInventory("BandageDressing");
 		player.GetInventory().CreateInInventory("Chemlight_White");
