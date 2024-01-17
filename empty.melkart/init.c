fa6f1006f1f5ca0610655ffe55057db698e9efdd
Jeff Day <jeffday@gmail.com>
2024-01-16 20:14:26 -0600

    update melkart to 1.23

--- empty.melkart/init.c
+++ empty.melkart/init.c
@@ -41,28 +41,16 @@ class CustomMission: MissionServer
 		}
 	}
 
-	void spawnItemsOnPlayer(PlayerBase player, TStringArray items) {
-        for(int i = 0; i < items.Count(); i++) {
-             player.GetInventory().CreateInInventory(items[i]);
-        }
-    }
-
 	override PlayerBase CreateCharacter(PlayerIdentity identity, vector pos, ParamsReadContext ctx, string characterName)
 	{
 		Entity playerEnt;
-		playerEnt = GetGame().CreatePlayer(identity, characterName, pos, 0, "NONE");//Creates random player
-		Class.CastTo(m_player, playerEnt);
+		playerEnt = GetGame().CreatePlayer( identity, characterName, pos, 0, "NONE" );
+		Class.CastTo( m_player, playerEnt );
 
-		GetGame().SelectPlayer(identity, m_player);
+		GetGame().SelectPlayer( identity, m_player );
 
 		return m_player;
 	}
-
-    void SpawnJeffSetup(PlayerBase player) {
-        autoptr TStringArray clothes = {"beanie_blackred","ThickFramesGlasses","Hoodie_Sport1","SlacksPants_Khaki","SK8_Sneakers_Black"};
-
-        spawnItemsOnPlayer(player, clothes);
-    }
 
 	override void StartingEquipSetup(PlayerBase player, bool clothesChosen)
 	{
@@ -74,19 +62,25 @@ class CustomMission: MissionServer
 		itemClothing = player.FindAttachmentBySlotName( "Body" );
 		if ( itemClothing )
 		{
-			player.RemoveAllItems();
-            
-            SpawnJeffSetup(player);
-            
-            itemEnt = player.GetInventory().CreateInInventory( "BandageDressing" );
-			itemEnt = player.GetInventory().CreateInInventory( "Chemlight_White" );
-            
-            itemEnt = player.GetInventory().CreateInInventory( "CanOpener" );
-            itemEnt = player.GetInventory().CreateInInventory( "SodaCan_Cola" );
-        	itemEnt = player.GetInventory().CreateInInventory( "SodaCan_Spite" );
-            itemEnt = player.GetInventory().CreateInInventory( "BakedBeansCan" );
-            itemEnt = player.GetInventory().CreateInInventory( "SpaghettiCan" );
-		} 
+			SetRandomHealth( itemClothing );
+			
+			itemEnt = itemClothing.GetInventory().CreateInInventory( "BandageDressing" );
+			if ( Class.CastTo( itemBs, itemEnt ) )
+				itemBs.SetQuantity( 2 );
+			player.SetQuickBarEntityShortcut(itemEnt, 2);
+
+			string chemlightArray[] = { "Chemlight_White", "Chemlight_Yellow", "Chemlight_Green", "Chemlight_Red" };
+			int rndIndex = Math.RandomInt( 0, 4 );
+			itemEnt = itemClothing.GetInventory().CreateInInventory( chemlightArray[rndIndex] );
+			player.SetQuickBarEntityShortcut(itemEnt, 1);
+			SetRandomHealth( itemEnt );
+		}
+		
+		itemClothing = player.FindAttachmentBySlotName( "Legs" );
+		if ( itemClothing )
+			SetRandomHealth( itemClothing );
+		
+		itemClothing = player.FindAttachmentBySlotName( "Feet" );
 	}
 };
 
